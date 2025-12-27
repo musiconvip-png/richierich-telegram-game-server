cf8caa33d1b8ddaa7be356c0b1ae3bd901d4c973
musiconvip-png <musiconvip@googlemail.com>
2025-12-27 20:13:00 +0100

    Create setWebhook.js

---
+++ api/api/setWebhook.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  const BOT_TOKEN = process.env.BOT_TOKEN;
+  if (!BOT_TOKEN) return res.status(200).json({ ok: false });
+
+  const WEBHOOK_URL = "https://richierich-telegram-game-server.vercel.app/api/webhook";
+
+  const r = await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/setWebhook`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ url: WEBHOOK_URL }),
+  });
+
+  const data = await r.json();
+  return res.status(200).json(data);
+}
